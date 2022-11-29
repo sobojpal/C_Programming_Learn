12b873432d1998e732e95d076d41d81ae6442635
sobojpal <sobojpal70@gmail.com>
2022-11-29 20:41:00 +0600

    data type size

---
+++ data type size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    float f;
+    double d;
+    char c;
+
+    printf("Size of integer : %d byte\n",sizeof(i));
+    printf("Size of float : %d byte\n",sizeof(f));
+    printf("Size of double : %d byte\n",sizeof(d));
+    printf("Size of char : %d byte\n",sizeof(c));
+}
