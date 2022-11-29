cc40b6d812610f0bdb013263760a2615f70734a6
sobojpal <sobojpal70@gmail.com>
2022-11-29 20:18:31 +0600

    prinf and scanf

---
+++ printf and scanf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    printf("Enter two intiger number : ");
+    scanf("%d %d",&num1 , &num2);
+
+    printf("Your intiger number is : %d %d\n", num1, num2);
+
+    return 0;
+}
