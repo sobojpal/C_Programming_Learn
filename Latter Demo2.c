b195b5e41af10591a45c3d5e8e6bf0dd3d1b73c1
sobojpal <sobojpal70@gmail.com>
2022-11-30 23:08:35 +0600

    upper to lowercases latter
    uppercases to lowercases latter without useing library function.

---
+++ Latter Demo2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char upper;
+    printf("Enter any uppercases latter : ");
+    scanf("%c",&upper);
+
+    printf("The lowercases latter : %c",upper+32);
+    return 0;
+}
