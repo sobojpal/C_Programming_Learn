a1e389f7a792cda5285eff903ff7ba7c1389a48a
sobojpal <sobojpal70@gmail.com>
2022-11-30 22:03:20 +0600

    data type sizr

--- data type size.c
+++ data type size.c
@@ -7,11 +7,8 @@ int main()
     char c;
 
     printf("Size of integer : %d byte\n",sizeof(i));
-
     printf("Size of float : %d byte\n",sizeof(f));
-
     printf("Size of double : %d byte\n",sizeof(d));
-
     printf("Size of char : %d byte\n",sizeof(c));
 
     return 0;
