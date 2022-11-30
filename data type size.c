245d6708a761714383635eda526129db7ecfc0bf
sobojpal <sobojpal70@gmail.com>
2022-11-30 22:01:38 +0600

    ASCII Value into Character

--- data type size.c
+++ data type size.c
@@ -7,7 +7,13 @@ int main()
     char c;
 
     printf("Size of integer : %d byte\n",sizeof(i));
+
     printf("Size of float : %d byte\n",sizeof(f));
+
     printf("Size of double : %d byte\n",sizeof(d));
+
     printf("Size of char : %d byte\n",sizeof(c));
+
+    return 0;
+
 }
