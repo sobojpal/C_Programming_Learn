c3c40c390fbdd7827f12e1a08a2e53768a53c735
sobojpal <sobojpal70@gmail.com>
2022-12-01 21:43:33 +0600

    Octal to Hexa-decimal

---
+++ octal to hexa-decimal.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int number ;
+    printf("Enter any Octal number : ");
+    scanf("%o",&number);
+
+    printf("The Hexa-decimal number is : %x",number);
+    return 0;
+}
