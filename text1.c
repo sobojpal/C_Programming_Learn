1624466807ff2c812f3f776e8a0e8e246564e8b7
sobojpal <sobojpal70@gmail.com>
2022-12-01 21:50:59 +0600

    ২ টি পূর্ণ সংখ্যার যোগফল নির্ণয়

---
+++ text1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int num1, num2, sum;
+    printf("Enter tow numbers :");
+    scanf("%d %d",&num1, &num2);
+
+    sum = num1 + num2;
+    printf("sum is : %d",sum);
+    return 0;
+}
