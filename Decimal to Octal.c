719fd8f0ea8caf47fb1936c7bd64e7c7b86755a4
sobojpal <sobojpal70@gmail.com>
2022-12-01 21:36:57 +0600

    Decimal to Octal

---
+++ Decimal to Octal.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int number ;
+    printf("Enter any decimal number : ");
+    scanf("%d",&number);
+
+    printf("The octal number is : %o",number);
+    return 0;
+}
