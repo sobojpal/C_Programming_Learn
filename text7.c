49bf59ca8ee4e42a005ce75360dc7846c6bf80b2
sobojpal <sobojpal70@gmail.com>
2022-12-02 20:16:15 +0600

    Create text7.c

---
+++ text7.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float C,F;
+    printf("Enter centigrade : ");
+    scanf("%f",&C);
+
+    F = (C * 1.8) + 32;
+    printf("Faren : %f",F);
+    return 0;
+
+
+}
