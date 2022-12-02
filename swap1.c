e20ab66b97b26f51589103714fbda957aec8dba5
sobojpal <sobojpal70@gmail.com>
2022-12-02 20:45:24 +0600

    swap
    How to swap two numbers with temporary variable

---
+++ swap1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num1=10;
+    int num2=5;
+    int team;
+
+    team=num1;
+    num1=num2;
+    num2=team;
+    printf("num1 : %d\n",num1);
+    printf("num2 : %d\n",num2);
+    return 0;
+}
