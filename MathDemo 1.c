6110f4714cbf0978f935ca3be7637520000309c0
sobojpal <sobojpal70@gmail.com>
2022-12-03 23:01:12 +0600

    log10 input

--- MathDemo 1.c
+++ MathDemo 1.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    double x=10.5;
-    double result=log(x);
-    printf("log(%lf)=%lf",x,result);
-
+    double x=1;
+    double result=log10(x);
+    printf("log10(%lf)=%lf",x,result);
+    return 0;
 }
